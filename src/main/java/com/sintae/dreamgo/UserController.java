589fbd24126a200c00b6a4c7569d8282f671028e
noknok <kimstcool01@naver.com>
2017-03-21 01:53:22 +0900

    인증번호 발송까지 구현 ( 번호 입력해서 동일한지를 판단후 인증 성공 유무 판단)

--- src/main/java/com/sintae/dreamgo/UserController.java
+++ src/main/java/com/sintae/dreamgo/UserController.java
@@ -1,5 +1,9 @@
 package com.sintae.dreamgo;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
 import javax.annotation.Resource;
 
 import org.slf4j.Logger;
@@ -7,6 +11,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sintae.util.MailSender;
 
@@ -14,6 +20,8 @@ import com.sintae.util.MailSender;
 public class UserController {
 	private static final Logger logger = LoggerFactory.getLogger(InfoController.class);
 	
+	
+	//서비스객체 생성
 	@Resource(name="mailService")
 	private MailSender mailSender;
 	
@@ -32,12 +40,35 @@ public class UserController {
 			
 		}
 		
-		@RequestMapping(value="/doSending", method = RequestMethod.GET)
-		public void sending(){
-			
-			mailSender.sendMail("kimstcool01@naver.com", "인즈번호.", "인증번호 : 1123124");
+		
+		@RequestMapping(value="/doMailAuth", method = RequestMethod.POST)
+		public @ResponseBody Map<String, String> sending(@RequestParam("email") String email){
+			//boolean rex=true;
+			Map<String, String> resultMap = new HashMap<String, String>();
 			
 			logger.info("mailSeding page called!");
+			System.out.println("인증보낼 메일 주소:" + email);
+			
+			if (email==null){
+				//email이 작성되있으면 유효성 검사 시작
+			 //rex = Pattern.matches("[\\w\\~\\-\\.]+@[\\w\\~\\-]+(\\.[\\w\\~\\-]+)+",email.trim());
+			}
+			
+			double randomVal = Math.random();			
+			//6자리 인증번호 생성
+			
+			//메일함에서 인증번호를 입력하기 전
+				int authNumber = (int)(randomVal*999999)+000000;
+				//메일발송
+				boolean result = mailSender.sendMail(email, "인증번호입니다.",authNumber);
+				
+				
+				if(result){
+					resultMap.put("result", "success");
+				}else{
+					resultMap.put("result", "failed");	
+				}			
+			return resultMap;
 			
 		}
 	
--- src/main/java/com/sintae/util/MailSender.java
+++ src/main/java/com/sintae/util/MailSender.java
@@ -1,7 +1,13 @@
 package com.sintae.util;
 
+
+import javax.mail.MessagingException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMessage.RecipientType;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
@@ -15,14 +21,38 @@ public class MailSender {
 	    /**
 	     * 메일 보내는 메서드 
 	     * */
-	    public void sendMail(String to, String subject, String body) 
+	    public boolean sendMail(String to, String subject, int authNumber) 
 	    {
 	    	
-	        SimpleMailMessage message = new SimpleMailMessage();
-	        message.setTo(to);
-	        message.setSubject(subject);
-	        message.setText(body);
-	        mailSender.send(message);
+	        MimeMessage message = mailSender.createMimeMessage();
+	        try {
+	        	String html = ""
+	        			+ "<h2 style='background:silver; width:200px;'>"
+	        			+ "인증번호 : "+authNumber+""
+	        			+ "</h2>"
+	        			+ "<br/>"
+	        			+ "<form action='localhost/dreamgo/doMailAuth' method='POST'>"
+	        			+ "<h3>인증번호 입력</h3>"
+	        			+ "<input type='text' name='checkedNumber'/>"
+	        			+ "<button type='button'>확인</button>"
+	        			+ "</form>";
+	        	
+	        	
+				message.setFrom(new InternetAddress("kimstcool01@gmail.com"));//발신자
+				message.addRecipient(RecipientType.TO, new InternetAddress(to));//수신자
+				message.setSubject(subject,"UTF-8");//제목
+				message.setText(html,"UTF-8","html");
+				mailSender.send(message);
+
+				return true;
+			} catch (MessagingException e) {
+				e.printStackTrace();
+				return false;
+			} catch(MailException e){
+				e.printStackTrace();
+				return false;
+			}
+
 	    }
 
 	
