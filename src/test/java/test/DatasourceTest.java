d612d9753fe490d2a97cb0141f43031e004cb818
noknok <kimstcool01@naver.com>
2017-03-21 15:26:57 +0900

    datasource 등록 성공

--- src/test/java/test/DatasourceTest.java
+++ src/test/java/test/DatasourceTest.java
@@ -1,10 +1,32 @@
 package test;
 
-import org.junit.runner.RunWith;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import java.sql.Connection;
 
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+//root-context.xml,servelt-context.xml 에 등록된 빈 가져오기
 @RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(
+		locations={"file:src/main/webapp/WEB-INF/spring/**/*.xml"})
 public class DatasourceTest {
 
+	@Inject
+	private DataSource ds;
+	
+	@Test
+	public void test()throws Exception{
+		try{
+			Connection conn = ds.getConnection();
+			System.out.println(conn);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
 }
